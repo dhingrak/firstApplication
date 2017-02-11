177e7597afbd3181cf1766b0b5d43c4eccaa1f89
Kunal Dhingra <dhingrak@udml16.cs.uregina.ca>
2017-02-11 12:03:59 -0600

    testing

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -36,12 +36,12 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         nameTextField.delegate = self
         
         // Set up views if editing an existing Meal.
-        if let meal = meal {
+        /*if let meal = meal {
             navigationItem.title = meal.name
             nameTextField.text = meal.name
             photoImageView.image = meal.photo
             ratingControl.rating = meal.rating
-        }
+        }*/
         
         // Enable the Save button only if the text field has a valid Meal name.
         updateSaveButtonState()
